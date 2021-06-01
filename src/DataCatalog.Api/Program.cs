dd0c8324a10f7906bb43feb3522d7faa3fe8f0e0
Mads Svejstrup <msv@iterator-it.dk>
2021-06-01 16:00:26 +0200

    Removed KeyVault setup

--- src/DataCatalog.Api/Program.cs
+++ src/DataCatalog.Api/Program.cs
@@ -1,12 +1,8 @@
 using System;
-using DataCatalog.Api.Extensions;
 using DataCatalog.Common.Extensions;
 using DataCatalog.Common.Utils;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Azure.KeyVault;
-using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
@@ -57,21 +53,6 @@ namespace DataCatalog.Api
                         .ReadFrom.Configuration(hostingContext.Configuration)
                         .Enrich.FromLogContext()
                         .Enrich.WithEnvironment();
-                })
-                .ConfigureAppConfiguration((context, config) =>
-                {
-                    if (!context.HostingEnvironment.IsDevelopment())
-                    {
-                        var azureServiceTokenProvider = new AzureServiceTokenProvider();
-                        var keyVaultClient = new KeyVaultClient(
-                            new KeyVaultClient.AuthenticationCallback(
-                                azureServiceTokenProvider.KeyVaultTokenCallback));
-
-                        config.AddAzureKeyVault(
-                            configuration.GetValidatedStringValue("DataCatalogKeyVaultUrl"),
-                            keyVaultClient,
-                            new DefaultKeyVaultSecretManager());
-                    }
                 })
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
                 .Build();
