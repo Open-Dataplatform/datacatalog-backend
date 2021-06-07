ebf7c106d8fefe2f6b9667d61390f3aaf26c7d17
Mads Svejstrup <msv@iterator-it.dk>
2021-06-07 11:52:01 +0200

    Disabled service bus temporarily

--- src/DataCatalog.Common/Extensions/HostBuilderExtensions.cs
+++ src/DataCatalog.Common/Extensions/HostBuilderExtensions.cs
@@ -10,7 +10,7 @@ namespace DataCatalog.Common.Extensions
             builder.SetBasePath(Directory.GetCurrentDirectory());
             builder.AddJsonFile("appsettings.json", false, true);
             builder.AddJsonFile($"appsettings.{environmentName.FirstCharToUpper()}.json", true, true);
-            builder.AddJsonFile($"/vault/secrets/appsettings.secrets.json", false, true); // This file is injected when running on Kubernetes
+            builder.AddJsonFile($"/vault/secrets/appsettings.secrets.json", true, true); // This file is injected when running on Kubernetes
             builder.AddEnvironmentVariables();
             builder.AddCommandLine(commandLineArgs);
             return builder;
