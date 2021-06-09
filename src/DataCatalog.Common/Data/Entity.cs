87660a9c8cfe62a2a6f2f6d46af0b7dcd0dc06bb
Mads Svejstrup <msv@iterator-it.dk>
2021-06-09 14:22:22 +0200

    Implemented seeding in migrator app

--- src/DataCatalog.Common/Data/Entity.cs
+++ src/DataCatalog.Common/Data/Entity.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using DataCatalog.Common.Interfaces;
 
 namespace DataCatalog.Common.Data
 {
-    public abstract class Entity : Created
+    public abstract class Entity : Created, IGuidId
     {
         public Guid Id { get; set; }
         public DateTime ModifiedDate { get; set; }
