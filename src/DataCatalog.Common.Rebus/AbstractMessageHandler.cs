f60b69e0a95825f0b139392c04e38777bb24bbbb
Kasper Damgaard <kld@mjolner.dk>
2021-07-13 12:59:37 +0200

    Simplify failed message flow

--- src/DataCatalog.Common.Rebus/AbstractMessageHandler.cs
+++ src/DataCatalog.Common.Rebus/AbstractMessageHandler.cs
@@ -4,7 +4,9 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Rebus.Bus;
+using Rebus.Exceptions;
 using Rebus.Handlers;
+using Rebus.Messages;
 using Rebus.Retry.Simple;
 using Serilog.Context;
 
@@ -27,52 +29,28 @@ namespace DataCatalog.Common.Rebus
         
         public Task Handle(IFailed<T> message)
         {
-            var optionalHeaders = new Dictionary<string, string>();
-            int currentAttempt;
-            if (message.Headers != null && message.Headers.ContainsKey(RebusOptions.AttemptNumberKey))
-            {
-                var tryParseSuccess = int.TryParse(message.Headers[RebusOptions.AttemptNumberKey], out var attemptNumber);
-                if (!tryParseSuccess)
+            var maxDeferCount = _options.MaxAttempts;
+            var deferCount = Convert.ToInt32(message.Headers.GetValueOrDefault(Headers.DeferCount));
+            if (deferCount >= maxDeferCount) {
+                using (LogContext.PushProperty("Message", message, true))
                 {
-                    throw new ArgumentException("The header of message contained an " + RebusOptions.AttemptNumberKey + " with an unexpected format. Expected an int, value: " + message.Headers[RebusOptions.AttemptNumberKey]);
+                    _logger.LogError("Unable to deliver message. Forwarding message to the error queue");
                 }
-                currentAttempt = ++attemptNumber;
-                optionalHeaders.Add(RebusOptions.AttemptNumberKey, attemptNumber.ToString());
-                if (attemptNumber > _options.MaxAttempts)
-                {
-                    // To make the message end on the error queue, we throw an exception to fail the task
-                    // and make Rebus's SimpleRetryStrategy move the message to error queue.
-                    // Note that the implementation of SimpleRetryStrategy will make this second level
-                    // retry mechanism retry until the MaxAttempts is exceeded, like the ordinary message.
-                    //
-                    // Creator of Rebus, Mogens Heller Grabe, recommends this approach throwing exceptions
-                    // to mimic the Rebus' behaviour of forwarding messages to the error queue:
-                    // https://stackoverflow.com/a/43769002/617413
-                    using (LogContext.PushProperty("Message", message, true))
-                    {
-                        _logger.LogError("Unable to deliver message. Forwarding message to the error queue");
-                    }
-                    throw new RebusFailedMessageMaxAttemptsException();
-                }
+                return _bus.Advanced.TransportMessage.Deadletter($"Failed after {deferCount} deferrals\n\n{message.ErrorDescription}");
             }
-            else
-            {
-                currentAttempt = 1;
-                optionalHeaders.Add(RebusOptions.AttemptNumberKey, currentAttempt.ToString());
-            }
-
-            // if message failed to process, defer processing for ? minutes and try again
-            return DeferMessageAsync(message, optionalHeaders, currentAttempt);
+            
+            // if message failed to process, defer processing for _options.RetryInMinutes minutes and try again
+            return DeferMessageAsync(message, deferCount);
         }
         
-        private async Task DeferMessageAsync(IFailed<T> message, Dictionary<string, string> optionalHeaders, int currentAttempt)
+        private async Task DeferMessageAsync(IFailed<T> message, int currentAttempt)
         {
             using (LogContext.PushProperty("Message", message, true))
             {
                 _logger.LogWarning("Attempt number {AttemptNumber} failed. Deferring message for {RetryInMinutes} minutes before trying again", currentAttempt, _options.RetryInMinutes);
             }
 
-            await _bus.Advanced.TransportMessage.Defer(TimeSpan.FromMinutes(_options.RetryInMinutes), optionalHeaders);
+            await _bus.Advanced.TransportMessage.Defer(TimeSpan.FromMinutes(_options.RetryInMinutes));
         }
     }
 }
