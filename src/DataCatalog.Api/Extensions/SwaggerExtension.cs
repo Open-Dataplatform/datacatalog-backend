e56444f4a262fc7ed003a143e84869f4d655a4fa
Mads Svejstrup <msv@iterator-it.dk>
2021-07-14 16:57:56 +0200

    Use nswag instead of swashbuckle to generate swagger doc

--- src/DataCatalog.Api/Extensions/SwaggerExtension.cs
+++ src/DataCatalog.Api/Extensions/SwaggerExtension.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Collections.Generic;
 using System.IO;
 using Microsoft.AspNetCore.Mvc.Abstractions;
@@ -17,37 +16,51 @@ namespace DataCatalog.Api.Extensions
         /// <param name="services"></param>
         public static void AddSwagger(this IServiceCollection services)
         {
-            services.AddSwaggerGen(options =>
+            // services.AddSwaggerGen(options =>
+            // {
+            //     options.SwaggerDoc("v1.0", new OpenApiInfo
+            //     {
+            //         Title = "DataCatalog.Api endpoint",
+            //         Version = "v1.0",
+            //         Contact = new OpenApiContact()
+            //         {
+            //             Name = "Dataplatform team",
+            //             Email = "dataplatform@energinet.dk"
+            //         }
+            //     });
+            //     options.DocInclusionPredicate((docName, apiDesc) =>
+            //     {
+            //         var actionApiVersionModel = apiDesc.ActionDescriptor?.GetApiVersionModel();
+            //         if (actionApiVersionModel == null)
+            //         {
+            //             return true;
+            //         }
+            //         if (actionApiVersionModel.DeclaredApiVersions.Any())
+            //         {
+            //             return actionApiVersionModel.DeclaredApiVersions.Any(v => $"v{v.ToString()}" == docName);
+            //         }
+            //         return actionApiVersionModel.ImplementedApiVersions.Any(v => $"v{v.ToString()}" == docName);
+            //     });
+            //     options.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+            //     //options.OperationFilter<AddRequiredHeaderParameter>();
+
+            //     // Configure Swagger to use the xml documentation file
+            //     var xmlFile = Path.ChangeExtension(typeof(Startup).Assembly.Location, ".xml");
+            //     options.IncludeXmlComments(xmlFile);
+            // });
+
+            services.AddSwaggerDocument(config =>
             {
-                options.SwaggerDoc("v1.0", new OpenApiInfo
+                config.PostProcess = document =>
                 {
-                    Title = "DataCatalog.Api endpoint",
-                    Version = "v1.0",
-                    Contact = new OpenApiContact()
+                    document.Info.Version = "v1.0";
+                    document.Info.Title = "DataCatalog.Api endpoint";
+                    document.Info.Contact = new NSwag.OpenApiContact
                     {
                         Name = "Dataplatform team",
-                        Email = "dataplatform@energinet.dk"
-                    }
-                });
-                options.DocInclusionPredicate((docName, apiDesc) =>
-                {
-                    var actionApiVersionModel = apiDesc.ActionDescriptor?.GetApiVersionModel();
-                    if (actionApiVersionModel == null)
-                    {
-                        return true;
-                    }
-                    if (actionApiVersionModel.DeclaredApiVersions.Any())
-                    {
-                        return actionApiVersionModel.DeclaredApiVersions.Any(v => $"v{v.ToString()}" == docName);
-                    }
-                    return actionApiVersionModel.ImplementedApiVersions.Any(v => $"v{v.ToString()}" == docName);
-                });
-                options.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
-                //options.OperationFilter<AddRequiredHeaderParameter>();
-
-                // Configure Swagger to use the xml documentation file
-                var xmlFile = Path.ChangeExtension(typeof(Startup).Assembly.Location, ".xml");
-                options.IncludeXmlComments(xmlFile);
+                        Email = "dataplatform@energinet.dk",
+                    };
+                };
             });
         }
 
@@ -57,30 +70,33 @@ namespace DataCatalog.Api.Extensions
         /// <param name="app"></param>
         public static void UseCustomSwagger(this IApplicationBuilder app)
         {
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1.0/swagger.json", "API v1.0");
-            });
+            // app.UseSwagger();
+            // app.UseSwaggerUI(c =>
+            // {
+            //     c.SwaggerEndpoint("/swagger/v1.0/swagger.json", "API v1.0");
+            // });
+
+            app.UseOpenApi(config => config.DocumentName = "v1.0");
+            app.UseSwaggerUi3(config => config.DocExpansion = "list");
         }
         
         /// <summary>
         /// Header declaration on swagger documentation.
         /// </summary>
-        public class AddRequiredHeaderParameter : IOperationFilter
-        {
-            public void Apply(OpenApiOperation operation, OperationFilterContext context)
-            {
-                if (operation.Parameters == null)
-                    operation.Parameters = new List<OpenApiParameter>();
+        // public class AddRequiredHeaderParameter : IOperationFilter
+        // {
+        //     public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        //     {
+        //         if (operation.Parameters == null)
+        //             operation.Parameters = new List<OpenApiParameter>();
 
-                operation.Parameters.Add(new OpenApiParameter()
-                {
-                    Name = "X-Version",
-                    Description = "Request api version.",
-                    Required = true
-                });
-            }
-        }
+        //         operation.Parameters.Add(new OpenApiParameter()
+        //         {
+        //             Name = "X-Version",
+        //             Description = "Request api version.",
+        //             Required = true
+        //         });
+        //     }
+        // }
     }
 }
